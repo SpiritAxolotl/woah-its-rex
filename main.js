d85006e0ac89632d33e979b0b7d1b97dd0c7fbec
Amber <ambwuwu@gmail.com>
2023-11-26 18:45:12 -0700

    woah, its wulzy

--- main.js
+++ main.js
@@ -20,9 +20,6 @@ function init () {
         loadData();
     }
     localStorage.setItem("playedBefore", true);
-    if (mobileCheck()) {
-        document.getElementById("mobile").style.display = "block";
-    }
 }
 function createMine() {
     for (let i = 0; i < 1000; i++) {
@@ -91,6 +88,7 @@ function movePlayer(dir) {
         default:
             console.log("wrong key!!");
     }
+    document.getElementById("location").innerHTML = "X: " + curX + " | Y: -" + curY;
     displayArea();
     //saveData();
 }
@@ -102,7 +100,7 @@ document.addEventListener('keydown', (event) => {
 
   function displayArea() {
     let output ="";
-    let constraints = getDisplays();
+    let constraints = getParams(5, 5);
     for (let r = curY - constraints[2]; r <= curY + constraints[3]; r++) {
         for (let c = curX - constraints[0]; c <= curX + constraints[1]; c++) {
             output += mine[r][c];
@@ -114,28 +112,28 @@ document.addEventListener('keydown', (event) => {
     document.getElementById("mineResetProgress").innerHTML = blocksRevealed + "/1,000,000 Blocks Revealed";
     document.getElementById("blocksMined").innerHTML = totalMined + " Blocks Mined";
   }
-  function getDisplays() {
+  function getParams(distanceX, distanceY) {
     let displayLeft = 0;
     let displayRight = 0;
     let displayUp = 0;
     let displayDown = 0;
     if (curX > 5) {
-        displayLeft = 5;
+        displayLeft = distanceX;
     } else {
         displayLeft = curX;
     }
     if (curX < 994) {
-        displayRight = 5;
+        displayRight = distanceX;
     } else {
         displayRight = 999 - curX;
     }
     if (curY > 5) {
-        displayUp = 5;
+        displayUp = distanceY;
     } else {
         displayUp = curY;
     }
     if (curY < 994) {
-        displayDown = 5;
+        displayDown = distanceY;
     } else {
         displayDown = 999 - curY;
     }
@@ -150,13 +148,13 @@ document.addEventListener('keydown', (event) => {
             blocksRevealed++;
         }
     }
-    if (curX + 1 < 999) {
+    if (curX + 1 < 1000) {
         if (mine[curY][curX + 1] == "⬜") {
             mine[curY][curX + 1] = generateBlock();
             blocksRevealed++;
         }
     }
-    if (curY + 1 < 999) {
+    if (curY + 1 < 1000) {
         if (mine[curY + 1][curX] == "⬜") {
             mine[curY + 1][curX] = generateBlock();
             blocksRevealed++;
@@ -275,7 +273,7 @@ let probabilityTable = {
       let blockToGive = "";
       let summedProbability = 0;
       let chosenValue = Math.random();
-      chosenValue /= 1;
+      chosenValue /= 1000;
       for (var propertyName in probabilityTable) {
         summedProbability += probabilityTable[propertyName][0];
         if (chosenValue < summedProbability) {
@@ -284,14 +282,19 @@ let probabilityTable = {
         }
         }
         if (Math.round(1 / (probabilityTable[blockToGive][0])) > 75000000) {
+            spawnMessage(blockToGive);
             playSound("otherworldly");
         } else if (Math.round(1 / (probabilityTable[blockToGive][0])) >= 16000000){
+            spawnMessage(blockToGive);
             playSound("unfathomable");
         } else if (Math.round(1 / (probabilityTable[blockToGive][0])) >= 5000000) {
+            spawnMessage(blockToGive);
             playSound("enigmatic");
         } else if (Math.round(1 / (probabilityTable[blockToGive][0])) >= 2000000) {
+            spawnMessage(blockToGive);
             playSound("transcendent");
         } else if (1 / (probabilityTable[blockToGive][0]) >= 100000) {
+            spawnMessage(blockToGive);
             playSound("exotic");
         }
         return blockToGive;
@@ -307,7 +310,6 @@ function updateInventory(type, inv) {
 let names = ["Normal", "Electrified", "Radioactive", "Explosive"]
 function switchInventory(){ 
     document.getElementById(("inventory") + variant).style.display = "none";
-    console.log("clicked");
   if (variant == 4) {
     variant = 1;
   } else {
@@ -318,6 +320,8 @@ function switchInventory(){
 }
 
 function resetMine() {
+    clearInterval(loopTimer);
+    curDirection = "";
     mine = [];
     curX = 5;
     curY = 0;
@@ -346,7 +350,7 @@ function loadData() {
         }
     }
     totalMined = JSON.parse(localStorage.getItem("amountMined"));
-    document.getElementById("blocksMined").innerHTML = totalMined;
+    document.getElementById("blocksMined").innerHTML = totalMined + "Blocks Mined";
 }
 
 function playSound(type) {
@@ -372,18 +376,20 @@ function playSound(type) {
         audio.play();
   }
 
-  window.mobileCheck = function() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
-  };
-  console.log(mobileCheck());
 
-let directionTimer = null;
-function goDirection(state) {
-    clearInterval(directionTimer);
-    directionTimer = setInterval(movePlayer(state), 100)
-}
+  let loopTimer = null;
+  let curDirection = "";
+  function goDirection(direction) {
+    if (curDirection == direction) {
+        clearInterval(loopTimer);
+    } else {
+        clearInterval(loopTimer);
+        loopTimer = setInterval(movePlayer, 50, direction);
+        curDirection = direction;
+    }
+    
+    
+  }
 
 function createInventory() {
     for (var propertyName in probabilityTable) {
@@ -396,6 +402,12 @@ function createInventory() {
             document.getElementById(("inventory") + i).appendChild(tempElement);
         }
     }
+}
+function spawnMessage(block) {
+    document.getElementById("spawnMessage").innerHTML = block + " Has Spawned!<br>" + "1/" + Math.round(1 / (probabilityTable[block][0]));
+    setTimeout(() => {
+        document.getElementById("spawnMessage").innerHTML = "Spawn Messages Appear Here"
+      }, 10000);
 }
 
 
