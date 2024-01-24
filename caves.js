a68e692e9c9a376837f9ecec96638e7fd39b1f71
Amber <ambwuwu@gmail.com>
2024-01-23 17:38:02 -0700

    erm changes

--- caves.js
+++ caves.js
@@ -28,7 +28,7 @@ function generateCave(x, y, rate, reps, type) {
                         mineCaveBlock(c, r, caveType);
                 }
             }
-            let newRate = Math.round(Math.random() * 10) / 600;
+            let newRate = Math.round(Math.random() * 10) / 650;
             rate += newRate;
             reps++;
         }
